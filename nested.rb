299450009458ccc17eefe5143391bca59fc442b5
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-25 14:42:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -69,11 +69,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-		 {
-			 		:alan_kay[:known_for] => alans_new_info
-		 }
-
+     alans_new_info
 
 end
 
